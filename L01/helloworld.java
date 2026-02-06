cba705911c0d3a6c54d729eb414b58e4a00145bc
Vivek Trivedi <32205003+trivik19780@users.noreply.github.com>
2026-02-06 18:56:38 +0530

    c01

---
+++ L01/helloworld.java
@@ -0,0 +1,11 @@
+public class helloworld {
+  public static void clearScreen(){
+        System.out.print("\033[H\033[2J");
+          System.out.flush();
+
+  }
+  public static void main(String[] args) {
+        clearScreen();
+          System.out.println("Hello World");
+      }
+}
